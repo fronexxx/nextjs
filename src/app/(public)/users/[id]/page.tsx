797d0c4809efee347e76dfba9a654dd475332f6a
fronexxx <igorbotsii42@gmail.com>
2025-12-15 12:26:44 +0200

    lessons/lesson1/metadata

--- src/app/(public)/users/[id]/page.tsx
+++ src/app/(public)/users/[id]/page.tsx
@@ -1,8 +1,16 @@
 import {FC} from "react";
+import {Metadata} from "next";
 
 interface Props {
     params: { id: string };
 }
+
+export const generateMetadata = async ({params}: Props): Promise<Metadata> => {
+    const {id} = await params;
+    return {
+        title: "User Page Title" + id
+    }
+};
 
 const UserPage: FC<Props> = async ({params}) => {
 
