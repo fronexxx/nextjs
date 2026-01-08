9735752490a607b8b21c72c27ac63627b4e9a036
fronexxx <igorbotsii42@gmail.com>
2026-01-08 15:18:53 +0200

    lessons/lesson2/server-action-mutation

---
+++ src/app/submit/page.tsx
@@ -0,0 +1,9 @@
+const SubmitPage = () => {
+    return (
+        <div>
+                        
+        </div>
+    );
+};
+
+export default SubmitPage;
---
+++ src/server-actions/serverActions.tsx
@@ -0,0 +1,22 @@
+'use server'
+
+import sql from 'better-sqlite3';
+import {revalidatePath} from "next/cache";
+
+const db = sql('meals.db');
+
+export const saveMeal = async (formData: FormData) => {
+    // console.log('saveActions')
+    // console.log(formData)
+
+    const titleValue = formData.get('title');
+
+    db.prepare(`insert into meals(title) values(?)`)
+        .run(titleValue);
+    revalidatePath('/');
+}
+
+type Meal = { id: number, title: string };
+export const getMeals = async (): Promise<Meal[]> => {
+    return db.prepare<Meal[]>('select * from meals').all() as Meal[];
+}
