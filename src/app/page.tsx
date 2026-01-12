9130af96bcd21062d401945a57c25f9c12d438e7
fronexxx <igorbotsii42@gmail.com>
2026-01-12 14:11:01 +0200

    Homeworks/HW_2/Task-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import styles from "./page.module.css";
+import Form from "next/dist/client/app-dir/form";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+        <Form action={CreateCarAction}>
+            <input type="text" name={'brand'}/>
+            <input type="number" name={'price'}/>
+            <input type="number" name={'year'}/>
+        </Form>
     </div>
   );
 }
