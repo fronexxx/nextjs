a1b0b8c85176035ede0c210620228ed467fac93f
fronexxx <igorbotsii42@gmail.com>
2026-01-26 17:11:11 +0200

    commit

--- src/server-actions/serverActions.tsx
+++ src/server-actions/serverActions.tsx
@@ -8,6 +8,7 @@ const db = sql('meals.db');
 export const saveMeal = async (formData: FormData) => {
     // console.log('saveActions')
     // console.log(formData)
+    // console.log(formData)
 
     const titleValue = formData.get('title');
 
