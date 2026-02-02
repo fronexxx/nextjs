af830c95563d56610941486a8d51de90396c1ee4
fronexxx <igorbotsii42@gmail.com>
2026-02-02 15:34:11 +0200

    lessons/lesson3/ISR

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,8 +1,12 @@
 const UsersPage = async () => {
-    const users: any = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users: any = await fetch('https://jsonplaceholder.typicode.com/users', {cache: 'no-cache'})
         .then((response) => response.json());
+
+
     return (
         <div>
+            <h2>{Date.now()}</h2>
+
             {
                 users.map((user: any) => <div key={user.id}>{user.name}</div>)
             }
